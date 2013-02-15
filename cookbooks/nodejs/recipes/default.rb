a89b180cd95d0def252cfe79e55adece2285d91e
Daisuke Baba <dbaba@yourinventit.com>
2013-02-15 11:47:30 +0900

    Fixes issues where default.rb fails to install npm/nvm properly during Chef provisioning.

--- cookbooks/nodejs/recipes/default.rb
+++ cookbooks/nodejs/recipes/default.rb
@@ -1,25 +1,61 @@
-%w{curl nodejs}.each do |p|
+%w{curl git}.each do |p|
   package p do
     action :install
   end
 end
 
-execute "npm" do
+execute "nvm" do
+  user node[:current][:user]
+  command "
+    HOME=/home/#{node[:current][:user]} && curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+  "
+#  not_if "grep nvm /home/#{node[:current][:user]}/.profile"
+end
+
+bash "nodejs" do
+  user node[:current][:user]
+  code "
+    . /home/#{node[:current][:user]}/.profile
+    nvm install #{node[:nodejs][:version]}
+    nvm alias default #{node[:nodejs][:version]}
+  "
+  not_if "/home/#{node[:current][:user]}/.profile && node -v | grep #{node[:nodejs][:version]}"
+end
+
+bash "npm" do
+  user node[:current][:user]
+  cwd "/tmp"
   # clean=yes for avoiding /dev/tty error
-  command "curl -k https://npmjs.org/install.sh | sudo clean=yes sh"
+  code "
+    . /home/#{node[:current][:user]}/.profile
+    HOME=/home/#{node[:current][:user]} && curl -k https://npmjs.org/install.sh | clean=yes sh
+  "
   not_if "which npm"
 end
 
-execute "nvm" do
-  command "npm install -g nvm"
-  not_if "which nvm"
+bash "npm install moat" do
+  user node[:current][:user]
+  code "
+    . /home/#{node[:current][:user]}/.profile
+    HOME=/home/#{node[:current][:user]} && npm install -g moat
+  "
+  not_if "npm ls | grep moat"
 end
 
-execute "nodejs" do
-  command "
-    nvm download #{node[:nodejs][:version]}
-    nvm build #{node[:nodejs][:version]}
-    nvm install #{node[:nodejs][:version]}
+bash "npm install nodeunit" do
+  user node[:current][:user]
+  code "
+    . /home/#{node[:current][:user]}/.profile
+    HOME=/home/#{node[:current][:user]} && npm install -g nodeunit
   "
-  not_if "node -v | grep #{node[:nodejs][:version]}"
+  not_if "npm ls | grep nodeunit"
+end
+
+bash "npm install sinon" do
+  user node[:current][:user]
+  code "
+    . /home/#{node[:current][:user]}/.profile
+    HOME=/home/#{node[:current][:user]} && npm install -g sinon
+  "
+  not_if "npm ls | grep sinon"
 end
